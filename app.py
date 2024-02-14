e44e91703145d3c3cb4c019efac507eb3fae437b
vishakha-dhumal_psl <vishakha_dhumal@persistent.com>
2024-02-14 16:26:13 +0530

    added a comment

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import os
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
+#coomment added
+
 student_files = [doc for doc in os.listdir() if doc.endswith('.txt')]
 student_notes = [open(_file, encoding='utf-8').read()
                  for _file in student_files]
